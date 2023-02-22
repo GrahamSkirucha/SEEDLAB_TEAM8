8f3bd0e77b423761422ca59570acd6f6a213ccb8
BobtheElf <74628352+BobtheElf@users.noreply.github.com>
2023-02-22 14:59:10 -0700

    Encoder reader using library plus other stuff - Jaron O'Grady

---
+++ motorReader/motorReader.ino
@@ -0,0 +1,21 @@
+//copied from one of the example codes in the zip file
+#include <Encoder.h>
+//make sure the encoder is connected to 5 and 6
+#define ENCODER_USE_INTERRUPTS
+Encoder myEnc(2,3);
+int lastRead;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lastRead = 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int encoderRead = myEnc.read();
+  if(encoderRead != lastRead){
+    Serial.println(encoderRead);
+    lastRead = encoderRead;
+  }
+  
+}
