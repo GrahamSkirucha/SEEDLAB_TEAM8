e242115219e195aad5bab6b225647deb9c2e69e0
ryanyoung8456 <125421231+ryanyoung8456@users.noreply.github.com>
2023-02-27 14:50:28 -0700

    Update quadDetect.py

--- Miniproject/Final Deliverables/quadDetect.py
+++ Miniproject/Final Deliverables/quadDetect.py
@@ -7,81 +7,57 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 
+#Initializes camera and aruco dictionary
+cap = cv2.VideoCapture(0)
 
-def cv_exercise6():
-    
-    #Initializes camera and aruco dictionary
-    cap = cv2.VideoCapture(0)
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
 
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-    
-    #Continously takes pictures
-    while (True):
-        
-        ret,frame = cap.read()
-        #frame.resolution = (1280,720)
-        greyscaled = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        param = cv2.aruco.DetectorParameters_create()
-        corners,ids,rejects = cv2.aruco.detectMarkers(greyscaled,aruco_dict)
-        cv2.imshow('Live Video',greyscaled)
-        #Checks if markers are detected
-        if ids is None:
-            print("No markers found")
-            
-        
-        else:
-#            print(len(corners))
-            #image_with_markers = cv2.aruco.drawDetectedMarkers(greyscaled, aruco_dict, param)
-            #cv2.imshow('Live Video',image_with_markers)
-            
-            #Defines corners of marker if present
-            corners = np.array(corners).reshape(4,2)
-            top_left,top_right,bottom_right,bottom_left = corners
-            
+#Continously takes pictures
+while (True):
 
-            
-            top_left[0] = int(top_left[0])
-            print(top_left[0])
-            top_left[1] = int(top_left[1])
-            top_right[0] = int(top_right[0])
-            top_right[1] = int(top_right[1])
-            bottom_right[0] = int(bottom_right[0])
-            bottom_right[1] = int(bottom_right[1])
-            bottom_left[0] = int(bottom_left[0])
-            bottom_left[1] = int(bottom_left[1])
-                
-            #Finds the angle to the marker
-#            angle = ((top_left[0]+top_right[0])/2-320)*(53.5/640)
-            #image_with_markers = cv2.aruco.drawDetectedMarkers(greyscaled, aruco_dict, param)
-            centerX = (top_left[0]+top_right[0])/2
-            centerY = (top_left[1]+top_right[1])/2
-            if centerX > 320 and centerY >240:
-                print('3pi/2')
-                greyscaled = cv2.putText(greyscaled,'3pi/2',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
-            elif centerX < 320 and centerY >240:
-                print('pi')
-                greyscaled = cv2.putText(greyscaled,'pi',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
-            elif centerX < 320 and centerY <240:
-                print('pi/2')
-                greyscaled = cv2.putText(greyscaled,'pi/2',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
-            elif centerX > 320 and centerY <240:
-                print('0')
-                greyscaled = cv2.putText(greyscaled,'0',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
-            cv2.imshow('Live Video',greyscaled)
+    ret,frame = cap.read()
+    corners,ids,rejects = cv2.aruco.detectMarkers(frame,aruco_dict)
+    cv2.imshow('Live Video',frame)
+    #Checks if markers are detected
+    if ids is None:
+        print("No markers found")
 
-            #Displays text
-#            print("Aruco marker ID detected:",ids)
-#            print("Angle from marker:",angle)
-            #greyscaled = cv2.putText(greyscaled,str(angle),(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,255,255),3)
 
-        if cv2.waitKey(1)&0xFF==ord('q'):
-            break
-          
-        #Displays live video
-        #cv2.imshow('Live Video',greyscaled)  
- 
-       
-    cap.release()
-    cv2.destroyAllWindows()
+    else:
+        #Defines corners of marker if present
+        corners = np.array(corners).reshape(4,2)
+        top_left,top_right,bottom_right,bottom_left = corners
 
-cv_exercise6()
+        top_left[0] = int(top_left[0])
+        print(top_left[0])
+        top_left[1] = int(top_left[1])
+        top_right[0] = int(top_right[0])
+        top_right[1] = int(top_right[1])
+        bottom_right[0] = int(bottom_right[0])
+        bottom_right[1] = int(bottom_right[1])
+        bottom_left[0] = int(bottom_left[0])
+        bottom_left[1] = int(bottom_left[1])
+
+        centerX = (top_left[0]+top_right[0])/2
+        centerY = (top_left[1]+top_right[1])/2
+
+        #Checks Quadrants
+        if centerX > 320 and centerY >240:
+            frame = cv2.putText(greyscaled,'3pi/2',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
+            quadrant = '3pi/2';
+        elif centerX < 320 and centerY >240:
+            frame = cv2.putText(frame,'pi',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
+            quadrant = 'pi';
+        elif centerX < 320 and centerY <240:
+            frame = cv2.putText(frame,'pi/2',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
+            quadrant = 'pi/2';
+        elif centerX > 320 and centerY <240:
+            frame = cv2.putText(frame,'0',(100,400),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,0),3)
+            quadrant = '0';
+        cv2.imshow('Live Video',frame)
+
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
